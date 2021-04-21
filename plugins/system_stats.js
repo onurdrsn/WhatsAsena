50c83787ceaf8efc54ad66fcbf18d9f6f3e0dab6
Onur Dursun <58361733+onur55-tr@users.noreply.github.com>
2021-04-21 14:17:30 +0300

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -19,13 +19,29 @@ const Lang = Language.getString('system_stats');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'onr', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
         if (Config.ALIVEMSG == 'default') {
             await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster' , MessageType.text);
         }
+        else if (Config.ALIVEMSG == 'SELO') {
+            await message.client.sendMessage(
+            message.jid, 
+            fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/VID-20210228-WA0022.mp4"),
+            MessageType.video, 
+            { mimetype: Mimetype.mpeg, caption: "```WhatsAsena Founder SELO 💑 ZEYNO İçin Çalışıyor!```\n\n*Version:* 0.27.9 - Dev\n*Grade:* Founder\n*AIPackages:* Xteam / Eva / DeepAI / WhatsAsena / RTDA\n*DeepAI Version:* ```1.0.17```\n*XTeam Verison:* ```4.2```" }
+        )
+        }   
+        else if (Config.ALIVEMSG == 'ONUR') {
+            await message.client.sendMessage(
+            message.jid, 
+            fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/VID-20210402-WA0119.mp4"),
+            MessageType.video, 
+            { mimetype: Mimetype.mpeg, caption: "WhatsAsena Founder R25 İçin Çalışıyor!\n\n*Version:* 0.24.7 - Dev\n*Grade:* Founder\n*AIPackages:*Xteam / Eva / DeepAI / WhatsAsena / RTDA\n*DeepAI Version:* ```1.0.17```\n*XTeam Verison:* ```4.2```" }
+        )
+        }
         else {
-            await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by WhatsAsena*', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by ONUR ♥ MELİSA*', MessageType.text);
         }
     }));
 
@@ -39,13 +55,29 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'onr', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
-        if (Config.ALIVEMSG == 'default') {
+       if (Config.ALIVEMSG == 'default') {
             await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster' , MessageType.text);
         }
+        else if (Config.ALIVEMSG == 'SELO') {
+            await message.client.sendMessage(
+            message.jid, 
+            fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/VID-20210228-WA0022.mp4"),
+            MessageType.video, 
+            { mimetype: Mimetype.mpeg, caption: "```WhatsAsena Founder SELO 💑 ZEYNO İçin Çalışıyor!```\n\n*Version:* 0.27.9 - Dev\n*Grade:* Founder\n*AIPackages:* Xteam / Eva / DeepAI / WhatsAsena / RTDA\n*DeepAI Version:* ```1.0.17```\n*XTeam Verison:* ```4.2```" }
+        )
+        }   
+        else if (Config.ALIVEMSG == 'ONUR') {
+            await message.client.sendMessage(
+            message.jid, 
+            fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/VID-20210402-WA0119.mp4"),
+            MessageType.video, 
+            { mimetype: Mimetype.mpeg, caption: "WhatsAsena Founder R25 İçin Çalışıyor!\n\n*Version:* 0.24.7 - Dev\n*Grade:* Founder\n*AIPackages:*Xteam / Eva / DeepAI / WhatsAsena / RTDA\n*DeepAI Version:* ```1.0.17```\n*XTeam Verison:* ```4.2```" }
+        )
+        }
         else {
-            await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by WhatsAsena*', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by ONUR ♥ MELİSA*', MessageType.text);
         }
     }));
 
